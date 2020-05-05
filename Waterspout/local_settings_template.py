8f5a0e5d0909f27cc1efed74a1ab3004a4fdd7ff
Nick Santos <ns-ffc_api_client@maskedmail.net>
2020-05-05 14:09:03 -0700

    initial commit with skeleton Django project

---
+++ Waterspout/local_settings_template.py
@@ -0,0 +1,5 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'FGUIA0t8943uijopkInp(foUSYNL;KAFAS;JLKFLK'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
---
+++ waterspout_api/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WaterspoutApiConfig(AppConfig):
+    name = 'waterspout_api'
