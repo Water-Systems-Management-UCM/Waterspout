13feec55b52586b5088933295651d148efd2cc98
Nick Santos <ns-ffc_api_client@maskedmail.net>
2020-06-03 17:23:23 -0700

    Rebuilt initial migrations and including Python 3.7 Dapper Windows binaries

--- waterspout_api/admin.py
+++ waterspout_api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from waterspout_api import models
 # Register your models here.
+
+admin.site.register(models.Organization)
+admin.site.register(models.ModelArea)
+admin.site.register(models.RegionGroup)
+admin.site.register(models.Region)
+admin.site.register(models.CropGroup)
+admin.site.register(models.Crop)
+admin.site.register(models.CalibrationSet)
+admin.site.register(models.CalibratedParameter)
+admin.site.register(models.ModelRun)
+admin.site.register(models.RegionModification)
---
+++ waterspout_api/includes/README.md
@@ -0,0 +1,3 @@
+This folder is for external files from us or other sources that we need
+to deploy without a pip install. Primarily this is for Dapper, which will
+be built with Cython via CI/CD and then downloaded to this folder upon install
