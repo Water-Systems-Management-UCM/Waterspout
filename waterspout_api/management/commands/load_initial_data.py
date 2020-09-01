83f0f304400f74e35f5c65cbe3f13d37f8ed51e7
Nick Santos <ns-ffc_api_client@maskedmail.net>
2020-09-01 14:57:37 -0700

    Loading/setup fixes

--- waterspout_api/management/commands/load_initial_data.py
+++ waterspout_api/management/commands/load_initial_data.py
@@ -6,14 +6,18 @@ from waterspout_api import load
 
 log = logging.getLogger("__name__")
 
+
 class Command(BaseCommand):
-	help = 'Loads default data into the database. Currently loads data for the DAP model'
+	help = 'Loads default data into the database. Provide the region name to load (dap, agwa) as the --region argument'
 
 	def add_arguments(self, parser):
-		parser.add_argument('--region', nargs='+', type=str, dest="region", default=False,)
+		parser.add_argument('--region', nargs='+', type=str, dest="region", default=None,)
 
 	def handle(self, *args, **options):
 		log.info("Loading data to database")
+
+		if not options["region"] or not options["region"][0] or options["region"][0] == "":
+			raise ValueError("Can't proceed - no region name to load")
 
 		region_name = options["region"][0]
 		log.info(f"Region {region_name}")
