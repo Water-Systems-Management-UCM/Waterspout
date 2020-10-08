5e5595fa4aa37d487c1495d58ba32d515f71e1f0
Nick Santos <ns-ffc_api_client@maskedmail.net>
2020-10-08 14:26:52 -0700

    Update to permissions for model creation, etc - still needs new tests for deletion

--- waterspout_api/permissions.py
+++ waterspout_api/permissions.py
@@ -30,21 +30,23 @@ class IsInSameOrganization(permissions.BasePermission):
 	def _check_org_info(self, request_user, request_data, view):
 		# get the item ID, as well as the class of the item so we can look the item up.
 		# We assume this permission is only used for items that have an "organization" foreign key
-		item_id = view.kwargs['pk']
-		item_class = view.serializer_class.Meta.model
-		try:
-			item = item_class.objects.get(pk=item_id)
-		except item_class.DoesNotExist:
-			return PermissionError("Model Run doesn't exist")
+		if "pk" in view.kwargs:  # then we're checking against an existing object
+			item_id = view.kwargs['pk']
+			item_class = view.serializer_class.Meta.model
+			try:
+				item = item_class.objects.get(pk=item_id)
+			except item_class.DoesNotExist:
+				return PermissionError("Model Run doesn't exist")
 
-		# object creation - verges on validation, but is related to permissions
-		#if type(request_data) is not dict:
-		#	request_data = json.loads(request_data)
+			organization = item.organization
+		else:  # we're creating an object - check what org they specify instead of the org of the object
+			if type(request_data) is not dict:
+				request_data = json.loads(request_data)
+			organization = models.Organization.objects.get(id=int(request_data["organization"]))
 
 		#log.debug(f"Make Model Run Request: ${request_data}")
 
 		# Check Permissions
-		organization = item.organization
 		if not organization.has_member(request_user):
 			log.error("User is not a member of the specified organization and cannot create or modify model runs within it")
 			raise PermissionDenied(
