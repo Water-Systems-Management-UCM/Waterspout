4b32524b0f54c7da24f614b0a86c3330e6ce2b09
Nick Santos <ns-ffc_api_client@maskedmail.net>
2020-12-01 09:54:00 -0800

    Trying to test new code for processing runs

--- waterspout_api/management/commands/process_runs.py
+++ waterspout_api/management/commands/process_runs.py
@@ -2,7 +2,10 @@ import logging
 import time
 import traceback
 
+from django.db import connection
 from django.core.management.base import BaseCommand, CommandError
+from django.db.models import F, RowRange, Window
+from django.db.models.functions import Rank, ExtractYear
 
 from waterspout_api import models
 from Waterspout import settings
@@ -38,6 +41,24 @@ class Command(BaseCommand):
 
 	def _get_runs(self):
 		log.debug("Checking for new model runs")
-		new_runs = models.ModelRun.objects.filter(ready=True, running=False, complete=False)\
-											.order_by('date_submitted')
+
+		if "sqlite3" not in connection.settings_dict["ENGINE"]:  # window functions in django don't work on sqlite
+
+			# the window function is used to ensure fairness, providing the ability to merge someone's newer runs with
+			# someone else's older runs if the first person has lots of runs. Prevents someone from monopolizing the
+			# run processor with API calls - instead, new people
+			window_func = Window(
+				expression=Rank('date_submitted'),
+				partition_by=F('user_id'),
+				output_field=F('rank'),
+				order_by=F('date_submitted').asc(),
+			),
+
+			new_runs = models.ModelRun.objects.filter(ready=True, running=False, complete=False).annotate(window_func)\
+												.order_by('rank')[:8]  # only select up to 8 at a time so that if someone
+																	# submits a bunch over the API, we pause long enough to get new ones again soon
+		else:
+			new_runs = models.ModelRun.objects.filter(ready=True, running=False, complete=False) \
+				.order_by('date_submitted')
+
 		self._waiting_runs = new_runs
