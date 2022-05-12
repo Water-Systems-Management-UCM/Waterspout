55be4a40f0786786fa975b5cce35de39719f55c5
Nick Santos <nickrsan@users.noreply.github.com>
2022-05-12 08:30:56 -0700

    Make model area preferences available on listing instead of detail to support user preference setting

--- waterspout_api/serializers.py
+++ waterspout_api/serializers.py
@@ -264,12 +264,12 @@ class ModelAreaSerializer(ModelActionSerializer):
 	class Meta:
 		model = models.ModelArea
 		_base_fields = ["id", "organization_id", "name", "description", "map_center_latitude",
-		                "map_center_longitude", "map_default_zoom", "model_defaults"]
+		                "map_center_longitude", "map_default_zoom", "model_defaults", "preferences"]
 		fields = _base_fields
 		action_fields = {  # only send model results in detail view - that way the listing doesn't send massive amount
 			"retrieve": {     # of data, but we only need to load the specific model run again to get the results
 				"fields": _base_fields + ["main_help_page_content", "calibration_data", "rainfall_data", "input_data",
-				                          "crop_set", "region_set", "region_group_sets", "preferences",
+				                          "crop_set", "region_set", "region_group_sets",
 		                                    "supports_rainfall", "supports_irrigation", "background_code"]
 			}
 		}
