70116080e5d2f494e2083626bfff908ee019ae64
mtapia01 <michaeltapia80@yahoo.com>
2025-03-27 12:38:32 -0700

    Feat: Added new regions for california model

--- waterspout_api/load/__init__.py
+++ waterspout_api/load/__init__.py
@@ -1,3 +1,3 @@
 # Core loading code - to be called by DAP and AGWA loading code directly
 
-from . import core, general, agwa, dap, ca_cv, ca_drought
+from . import core, general, agwa, dap, ca_cv, ca_drought, cali
